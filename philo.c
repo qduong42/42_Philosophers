d64b94ed0ddcb127851438dd7c4aab9dc8b42eae
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-05-01 00:08:17 +0200

    nothing much

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: qduong <qduong@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 18:37:30 by qduong            #+#    #+#             */
-/*   Updated: 2022/04/30 00:26:13 by qduong           ###   ########.fr       */
+/*   Updated: 2022/04/30 23:42:48 by qduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,55 @@
 #include "philo.h"
 #include <unistd.h>
 
-typedef struct s_struct 
-{
-	int		philo_num;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		meal_amount;
-}				t_struct;
-
 //./philo time time time (optional)
+
+int main (int argc, char **argv)
+{
+	if (argc > 6 || argc < 5)
+	{
+		write(STDERR_FILENO, how, sizeof(how));
+		return (0);
+	}
+	t_struct info;
+	if (initial_arg_check(argv, &info))
+		write(STDERR_FILENO, how, sizeof(how));
+	parse_info(&info);
+}
+
+void*	routine(t_struct *info)
+{
+	
+	return (NULL);
+}
+
+void	parse_info(t_struct *info)
+{
+	int	i;
+
+	i = 0;
+	info->philos = malloc(info->philo_num * sizeof(t_philo));
+	while (i < info->philo_num)
+	{
+		info->philos->id = i + 1;
+		if (pthread_create(info->thread, NULL, &routine, &info) != 0)
+			return (1);
+		i++;
+	}
+	return ;
+}
+
+int initial_arg_check(char **argv, t_struct *info)
+{
+	if (atoi_n_check(argv[1], &info->philo_num)||
+	atoi_n_check(argv[2], &info->time_to_die)||
+	atoi_n_check(argv[3], &info->time_to_eat)||
+	atoi_n_check(argv[4], &info->time_to_sleep))
+		return (1);
+	if (argv[5])
+		if(atoi_n_check(argv[5], &info->meal_amount))
+			return (1);
+	return (0);
+}
 
 int	atoi_n_check(char *argv, int *a)
 {
@@ -46,29 +85,4 @@ int	atoi_n_check(char *argv, int *a)
 	}
 	*a = (int)result;
 	return (0);
-}
-
-int initial_arg_check(char **argv, t_struct *info)
-{
-	if (atoi_n_check(argv[1], &info->philo_num)||
-	atoi_n_check(argv[2], &info->time_to_die)||
-	atoi_n_check(argv[3], &info->time_to_eat)||
-	atoi_n_check(argv[4], &info->time_to_sleep))
-		return (1);
-	if (argv[5])
-		if(atoi_n_check(argv[5], &info->meal_amount))
-			return (1);
-	return (0);
-}
-
-int main (int argc, char **argv)
-{
-	if (argc > 6 || argc < 5)
-	{
-		write(STDERR_FILENO, how, sizeof(how));
-		return (0);
-	}
-	t_struct info;
-	if (initial_arg_check(argv, &info))
-		write(STDERR_FILENO, how, sizeof(how));
 }
