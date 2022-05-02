39d2e34f780fddf75c06776b21258cba590aafd8
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-05-02 02:57:51 +0200

    init to win it

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: qduong <qduong@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 18:37:30 by qduong            #+#    #+#             */
-/*   Updated: 2022/05/01 18:32:27 by qduong           ###   ########.fr       */
+/*   Updated: 2022/05/02 02:56:32 by qduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,14 @@
 #include "philo.h"
 #include <unistd.h>
 
-//./philo time time time (optional)
+//./philo time time time (optional) returns 1 for error
 
-int	main (int argc, char **argv)
+void*	routine(t_philo *phil)
 {
-	if (argc > 6 || argc < 5)
-	{
-		write(STDERR_FILENO, how, sizeof(how));
-		return (0);
-	}
-	t_struct info;
-	if (initial_arg_check(argv, &info))
-		write(STDERR_FILENO, how, sizeof(how));
-	parse_info(&info);
-	create_mutex(&info);
-}
-
-void*	routine(t_struct *info)
-{
-	
+	int	i;
+	i = 0;
+	if (phil->id % 2 == 0)
+			usleep(42);
 	return (NULL);
 }
 
@@ -43,9 +32,14 @@ int	create_mutex(t_struct *info)
 	i = 0;
 	while (i < info->philo_num)
 	{
-	if (pthread_mutex_init(&info->fork[i], NULL) != 0)
-		return (1);
+		if (pthread_mutex_init(&info->fork[i], NULL) != 0)
+			return (1);
+		i++;
 	}
+	if (pthread_mutex_init(&info->print, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&info->death, NULL) != 0)
+		return (1);
 }
 
 int	parse_info(t_struct *info)
@@ -59,12 +53,10 @@ int	parse_info(t_struct *info)
 		return (1);
 	while (i < info->philo_num) //ID will be 1, 2, 3 while i is 0, 1, 2
 	{
-		info->philos->id = i + 1;
-		if (pthread_create(info->thread, NULL, &routine, &info) != 0)
+		info->philos[i].id = i + 1;
+		if (pthread_create(info->thread, NULL, &routine, &info->philos[i]) != 0)
 			return (1);
 		i++;
-		if (info->philos->id % 2 == 0)
-			usleep(42);
 	}
 	return (0);
 }
@@ -102,5 +94,42 @@ int	atoi_n_check(char *argv, int *a)
 			return (1);
 	}
 	*a = (int)result;
+	return (0);
+}
+
+long long	your_time(void)
+{
+	_STRUCT_TIMEVAL time;
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+init_philo(t_struct *info)
+{
+	int	i;
+
+	i = 0;
+	while (i < info->philo_num)
+	{
+		info->philos[i].lastmeal = your_time();
+		i++;
+	}
+}
+
+int	main (int argc, char **argv)
+{
+	if (argc > 6 || argc < 5)
+	{
+		write(STDERR_FILENO, HOW, sizeof(HOW));
+		return (0);
+	}
+	t_struct info;
+	if (initial_arg_check(argv, &info))
+		write(STDERR_FILENO, HOW, sizeof(HOW));
+	if (parse_info(&info))
+		write(1, "Failed to create mallocs or thread", 35);
+	if (create_mutex(&info))
+		write(1, "Failed to create mutexes", 25);
+	init_philo(&info);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: qduong <qduong@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 10:58:53 by qduong            #+#    #+#             */
-/*   Updated: 2022/05/01 19:22:59 by qduong           ###   ########.fr       */
+/*   Updated: 2022/05/02 02:41:34 by qduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <sys/time.h>
 
-# define how "Use by ./philo philo_num time_to_die time_to_eat time_to_sleep\n"
+# define HOW "Use by ./philo philo_num time_to_die time_to_eat time_to_sleep\n"
 
 /*
 **Checks
@@ -29,22 +30,24 @@ int	atoi_n_check(char *argv, int *a);
 
 typedef struct s_struct 
 {
-	int			philo_num;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			meal_amount;
-	t_philo		*philos;
-	pthread_mutex_t		*fork;
-	pthread_t	thread;
+	int					philo_num; //set in initial_arg_Check
+	int					time_to_die; //set in initial_arg_Check
+	int					time_to_eat; //set in initial_arg_Check
+	int					time_to_sleep; //set in initial_arg_Check
+	int					meal_amount;  //set in initial_arg_Check
+	t_philo				*philos;
+	pthread_mutex_t		*fork; //malloced in parse info
+	pthread_mutex_t		print; //init in create_mutex
+	pthread_mutex_t		death; //init in create_mutex
+	pthread_t			thread; //stores info from pthread create
 }				t_struct;
 
 typedef struct s_philo
 {
-	int				id;
-	int				lastmeal;
-	pthread_mutex_t	left_fork;
-	pthread_mutex_t	right_fork;
+	int				id; //inited
+	int				lastmeal; //init_philo
+	pthread_mutex_t	*left_fork; //init philo
+	pthread_mutex_t	*right_fork; //init philo
 }				t_philo;
 
 # endif
